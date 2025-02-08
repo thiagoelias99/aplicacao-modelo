79f8db030e029bff90544b83a0228c33a56c8cb0
Thiago Elias <thiagoelias99@gmail.com>
2025-02-08 09:39:16 -0300

    header & hook fixes

--- app-modelo-1/src/hooks/use-local-storage.tsx
+++ app-modelo-1/src/hooks/use-local-storage.tsx
@@ -7,8 +7,11 @@ type keyValues = "acceptedCookies"
 export function useLocalStorage<T>(key: keyValues, initialValue: T) {
   const [storedValue, setStoredValue] = useState<T>(() => {
     try {
-      const item = window.localStorage.getItem(key)
-      return item ? JSON.parse(item) : initialValue
+      if (typeof window !== "undefined") {
+        const item = window.localStorage.getItem(key)
+        return item ? JSON.parse(item) : initialValue
+      }
+      return initialValue
     } catch (error) {
       console.error(error)
       return initialValue
@@ -17,9 +20,11 @@ export function useLocalStorage<T>(key: keyValues, initialValue: T) {
 
   const setValue = (value: T | ((val: T) => T)) => {
     try {
-      const valueToStore = value instanceof Function ? value(storedValue) : value
-      setStoredValue(valueToStore)
-      window.localStorage.setItem(key, JSON.stringify(valueToStore))
+      if (typeof window !== "undefined") {
+        const valueToStore = value instanceof Function ? value(storedValue) : value
+        setStoredValue(valueToStore)
+        window.localStorage.setItem(key, JSON.stringify(valueToStore))
+      }
     } catch (error) {
       console.error(error)
     }
